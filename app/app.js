5ecd280b2c6123c790f9db65206fb78b482be4a5
bayron <bayron_fabian@hotmail.com>
2018-11-29 22:08:03 -0300

    ahora si se agregan jquery fuente y main js

--- app/app.js
+++ app/app.js
@@ -1,15 +1,73 @@
-(function(){
-    var xhttp = new XMLHttpRequest();
-    var url = "http://127.0.0.1:8000/perros/";
-    var txtData= document.getElementById("txtData");
+(function() {
+    //ServiceWorker
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+                 .register('./service-worker.js')
+                 .then(function() {
+                     console.log('Service Worker Registered');
+                });
+    }
+})( );
 
-    xhttp.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200){
-            var data = this.responseText;
-            txtData.innerHTML = data;
+(  function() {
+    var app = {
+        perroEstadoFilter: document.getElementById( "perroEstadoFilter" ),
+        perroList: [],
+    }
+
+    var loadData = function() {
+        var xhttp = new XMLHttpRequest();
+        var url = "https://furby.pythonanywhere.com/perros/";
+
+        xhttp.onreadystatechange = function() {
+            if( this.readyState == 4 && this.status == 200 ){
+                console.log( this.responseText );
+                var data = JSON.parse( this.responseText );
+                displayPerros( data.results );
+                app.perroList = data.results;
+            }
+        }
+        xhttp.open( 'GET', url, true );
+        xhttp.send();
+    }
+
+    var displayPerros = function( perros ) {
+        var perrosContainer = document.getElementById( "perrosContainer");
+        perrosContainer.innerHTML = "";
+
+        for( let perro of perros ) {
+            var perroContainer = document.createElement( "div" );
+            var txtPerroNombre = document.createElement( "h3" );
+            var imgPerro = document.createElement( "img" );
+            var txtPerroCaracteristica = document.createElement( "p" );
+            var txtPerroRaza = document.createElement( "p" );
+            var txtPerroEstado = document.createElement( "p" );
+            perroContainer.className = "perroContainer";
+            txtPerroNombre.innerHTML = perro.nombre;
+            imgPerro.src = perro.imageUrl;
+            imgPerro.alt = perro.nombre;
+            txtPerroCaracteristica.innerHTML = perro.caracteristica;
+            txtPerroRaza.innerHTML = perro.raza;
+            txtPerroEstado.innerHTML = "<b>Estado: </b>" + perro.estado;
+            // Agrega hijos
+            perroContainer.appendChild( txtPerroNombre );
+            perroContainer.appendChild( imgPerro );
+            perroContainer.appendChild( txtPerroCaracteristica );
+            perroContainer.appendChild( txtPerroRaza );
+            perroContainer.appendChild( txtPerroEstado );
+            // Agregar contenedor 
+            perrosContainer.appendChild( perroContainer );
         }
     }
-    xhttp.open( "GET", url, true);
-    xhttp.send( );
 
+    app.perroEstadoFilter.addEventListener( "change", function( e ) {
+        var filteredPerros = app.perroList.filter( function( perro ) {
+            if( perro.estado == app.perroEstadoFilter.value ) {
+                return perro;
+            }
+        } );
+        displayPerros( filteredPerros );
+    } );
+    
+    loadData();
 } ) ( );
---
+++ app/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+	var imgItems = $('.slider li').length; // Slides a agregar
+	var imgPos = 1;
+
+	// Agrega pagina
+	for(i = 1; i <= imgItems; i++){
+		$('.pagination').append('<li><span class="fa fa-circle"></span></li>');
+	} 
+
+	$('.slider li').hide(); // ocultan slides
+	$('.slider li:first').show(); // Muestra 1r slide
+	$('.pagination li:first').css({'color': '#CD6E2E'}); //Estilo de pagina
+
+	// Ejecutan funciones
+	$('.pagination li').click(pagination);
+	$('.right span').click(nextSlider);
+	$('.left span').click(prevSlider);
+
+	setInterval(function(){
+		nextSlider();
+	}, 4000);
+
+	// Funciones
+
+	function pagination(){
+		var paginationPos = $(this).index() + 1; // Posicion de pagina 
+
+		$('.slider li').hide(); // Ocultan Slides
+		$('.slider li:nth-child('+ paginationPos +')').fadeIn(); // Muestra slide
+
+		// Estilo De pagina
+		$('.pagination li').css({'color': '#858585'});
+		$(this).css({'color': '#CD6E2E'});
+
+		imgPos = paginationPos;
+
+	}
+
+	function nextSlider(){
+		if( imgPos >= imgItems){imgPos = 1;} 
+		else {imgPos++;}
+
+		$('.pagination li').css({'color': '#858585'});
+		$('.pagination li:nth-child(' + imgPos +')').css({'color': '#CD6E2E'});
+
+		$('.slider li').hide(); // Ocultan Slides
+		$('.slider li:nth-child('+ imgPos +')').fadeIn(); // Muestra slide
+
+	}
+
+	function prevSlider(){
+		if( imgPos <= 1){imgPos = imgItems;} 
+		else {imgPos--;}
+
+		$('.pagination li').css({'color': '#858585'});
+		$('.pagination li:nth-child(' + imgPos +')').css({'color': '#CD6E2E'});
+
+		$('.slider li').hide(); // Ocultan Slides
+		$('.slider li:nth-child('+ imgPos +')').fadeIn(); // Muestra slide
+	}
+
+});
